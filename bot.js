841d35e0add7f7fb95bcaf22b37aa0a46554019d
Jennings <Joseph.Jennings@mines.sdsmt.edu>
2024-06-20 18:52:45 -0400

    commit

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ function respond() {
   if (BotResponse.length > 0) {
     this.res.writeHead(200);
     console.log(this.req.chunks["name"]);
-    setTimeout(postMessage(BotResponse), 3000);
+    //setTimeout(postMessage(BotResponse), 3000);
     this.res.end();
   } else {
     console.log("don't care");
